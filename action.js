df9c81c64b853044adea599288496b5201a5abf1
Amala1998 <123836016+Amala1998@users.noreply.github.com>
2023-02-05 05:14:27 +0300

    Update action.js

--- action.js
+++ action.js
@@ -1,1 +1,22 @@
+function show(){
+document.images[0].src="احبك.jpg";
 
+}
+function show1(){
+    document.images[1].src="اضحكتني.jpg";
+    }
+    function show2(){
+        document.images[2].src="مؤلم.jpg";
+        }
+        function show3(){
+            document.images[3].src="هذا رهيب.jpg";
+            } 
+            function show4(){
+                document.images[4].src="هذا ممتاز.jpg";
+                }
+                function show5(){
+                    document.images[5].src="حقا احبك.jpg";
+                    }
+                    function show6(){
+                        document.images[6].src="اتمنى لك حياه سعيده.jpg";
+                        }
